69d2a7d36e8824648b03610dece049f3a838df20
thybag <thybag@gmail.com>
2013-06-08 18:05:59 +0100

    Update tpl to integrate with jQuery if its available.

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
  /**
  * TPL provides an ultra light weight, super simple method for quickly doing HTML templating in javascript.
  * @author Carl Saggs
- * @version 0.2
+ * @version 0.3
  * @source https://github.com/thybag/base.js
  */
  (function(){
@@ -24,6 +24,7 @@
 		var results = tmp.children;
 		//if only one node, return as individual result
 		if(tmp.children.length===1)results = tmp.children[0];
+
 		return results;
 	}
 
@@ -53,4 +54,29 @@
 
 	//Add tpl to global scope
 	window.tpl = this;
+
+	// If jQuery is defined, enable tpl as a jquery plugin.
+	//
+	// Examples: 
+	//
+	//		$('#my_tpl').tpl({"name":"test"}).appendTo("body");
+	// 		var hello = $("<div>Hello {name}").tpl({"name":"test"});
+	//
+	if(typeof $ !== 'undefined'){
+		$.fn.tpl = function(options){
+			var templated = $();
+			$(this).each(function(){ 
+				if($(this).is('script')){
+					// If loading from template data from a script tag, template it, then remove each element from the script 
+					// element and add it to the results nodelist
+					$( tpl.template($(this).prop('innerHTML'), options) ).each(function(){ templated.push(this); });
+				}else{
+					// Else just template each element and add it to the nodelist
+					templated.push( tpl.template($(this).prop('outerHTML'), options)); 
+				} 
+			});
+			return templated;
+		}
+	}
+	
 }).call({});
